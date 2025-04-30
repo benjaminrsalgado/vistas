0fec34ba33b9c68d6ec44773aa94eb88f99b3209
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-04-29 19:42:31 -0600

    i add a HStack

--- vistas/ContentView.swift
+++ vistas/ContentView.swift
@@ -9,33 +9,6 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack(alignment: .top) {
-            Text("Top")
-                .font(.largeTitle)
-            Text("Aligned Bottom")
-                .font(.largeTitle)
-        }
-        HStack {
-            Text("Left")
-                .font(.title)
-                .foregroundColor(.blue)
-
-            Text("Right")
-                .font(.title)
-                .foregroundColor(.green)
-        }
-        .padding()
-        .background(Color.gray)
-        .cornerRadius(100)
-        .padding()
-        
-        HStack {
-            Text("Start")
-            Spacer()
-            Text("Center")
-            Spacer()
-            Text("End")
-        }
         VStack {
             HStack {
                 Text("Column 1 - Row 1")
