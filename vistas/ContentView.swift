eeb0be5eb4f713457515059bc64ef4ce3464e8b9
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-04-29 19:39:33 -0600

    i did some changes

--- vistas/ContentView.swift
+++ vistas/ContentView.swift
@@ -9,13 +9,44 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack(alignment: .top) {
+            Text("Top")
+                .font(.largeTitle)
+            Text("Aligned Bottom")
+                .font(.largeTitle)
+        }
+        HStack {
+            Text("Left")
+                .font(.title)
+                .foregroundColor(.blue)
+
+            Text("Right")
+                .font(.title)
+                .foregroundColor(.green)
         }
         .padding()
+        .background(Color.gray)
+        .cornerRadius(100)
+        .padding()
+        
+        HStack {
+            Text("Start")
+            Spacer()
+            Text("Center")
+            Spacer()
+            Text("End")
+        }
+        VStack {
+            HStack {
+                Text("Column 1 - Row 1")
+                Text("Column 2 - Row 1")
+            }
+
+            HStack {
+                Text("Column 1 - Row 2")
+                Text("Column 2 - Row 2")
+            }
+        }
     }
 }
 
